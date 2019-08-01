d9c31ceb1149415cdd9b94b48bb68ba80076853b
sweeney <martin@sweeney.io>
2019-08-01 17:56:40 +0100

    For next time

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s /Users/sweeney/src/github.com/sweeney/dotfiles/bash_profile .bash_profile
+ln -s /Users/sweeney/src/github.com/sweeney/dotfiles/bashrc .bashrc
+ln -s /Users/sweeney/src/github.com/sweeney/dotfiles/gitconfig .gitconfig
+ln -s /Users/sweeney/src/github.com/sweeney/dotfiles/plan .plan
+ln -s /Users/sweeney/src/github.com/sweeney/dotfiles/vimrc .vimrc
+ln -s /Users/sweeney/src/github.com/sweeney/dotfiles/ssh/config .ssh/config
